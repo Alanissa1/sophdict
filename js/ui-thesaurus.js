ad333b958f63aeb08efa274fdf7ff5b0ced19683
Alanissa1 <98144862+Alanissa1@users.noreply.github.com>
2026-07-22 19:02:25 +0300

    Add files via upload

--- js/ui-thesaurus.js
+++ js/ui-thesaurus.js
@@ -99,6 +99,7 @@ window.UIThesaurus = {
 
                             const mergedSimilar = [...syns, ...sims, ...phrases];
                             const mergedOpposite = [...ants, ...opps];
+                            const hasTags = mergedSimilar.length || rels.length || mergedOpposite.length || nears.length;
 
                             html += `
                                 <div class="sense-block">
@@ -107,6 +108,7 @@ window.UIThesaurus = {
                                         <div class="def-content-container">
                                             <div class="def-text">${def} <span class="tts-inline-target" data-text="${escapedDef}"></span></div>
                                             ${exHtml}
+                                            ${hasTags ? `
                                             <div class="tags-section expandable-wrapper">
                                                 <div class="tags-rows-container">
                                                     ${mergedSimilar.length ? `<div class="tags-row Similar-row"><span class="tags-label">Similar:</span>${mergedSimilar.map(s => `<span class="tag syn-tag" data-word="${s.wd}" tabindex="0">${s.html}</span>`).join('')}</div>` : ''}
@@ -117,10 +119,11 @@ window.UIThesaurus = {
                                                         <svg class="ignore-dark-override" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="currentColor"><path d="m480-347.33 175.33-175.34-47.66-46.66L480-441.67 352.33-569.33l-47.66 46.66L480-347.33ZM480-80q-82.33 0-155.33-31.5-73-31.5-127.34-85.83Q143-251.67 111.5-324.67T80-480q0-83 31.5-156t85.83-127q54.34-54 127.34-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82.33-31.5 155.33-31.5 73-85.5 127.34Q709-143 636-111.5T480-80Zm0-66.67q139.33 0 236.33-97.33t97-236q0-139.33-97-236.33t-236.33-97q-138.67 0-236 97-97.33 97-97.33 236.33 0 138.67 97.33 236 97.33 97.33 236 97.33ZM480-480Z"/></svg>
                                                     </button>
                                                 </div>
-                                            </div>
+                                            </div>` : ''}
                                         </div>
                                     </div>
                                 </div>`;
+
                         };
                         processNode(node);
                     });
